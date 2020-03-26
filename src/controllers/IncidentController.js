a08a9e9f78ae135902f80643f5f96456f20a1d31
Treinamento <Treinamento@RD723896.raiadrogasil.com>
2020-03-26 20:00:37 -0300

    terminando um crud

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -2,6 +2,16 @@ const connection = require('../database/connection')
 
 module.exports = {
 
+    // Lista todas as incidencias
+    async index(request, response) {
+
+        const incidents = await connection('incidents').select('*');
+        
+        return response.json(incidents);
+
+    },
+
+    // Criar uma nova incidencias
     async create(request, response) {
 
         const { title, description, value } = request.body;
@@ -15,6 +25,29 @@ module.exports = {
         })
         
         return response.json({ id })
+    },
+
+    // Deletar uma incidencias
+    async delete(request, response) {
+
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incidents = await connection('incidents')
+            .where('id' , id)
+            .select('ong_id')
+            .first();
+
+        if(incidents.ong_id != ong_id) {
+            return response.status(401).json({ erro: 'Operation not permitted.' })
+        }
+
+        await connection('incidents')
+            .where('id', id)
+            .delete();
+
+        return response.status(204).send();
+
     }
 
 }
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,8 @@ const routes = express.Router();
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create);
 
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
